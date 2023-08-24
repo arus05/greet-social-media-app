d3d9533fc5d96a61d3e2cc9530b5fec987b7aa4e
Arus <arus.zh0905@gmail.com>
2023-08-24 18:50:14 +0800

    fix LightBox import in Posts.jsx

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,14 +1,14 @@
 import Post from "./Post";
-import LightBox from "./Lightbox";
+import LightBox from "./LightBox";
 import { useState } from "react";
 
 const Posts = ({ posts }) => {
-  const [clickedPostImage, setClickedPostImage] = useState(null)
+  const [clickedPostImage, setClickedPostImage] = useState(null);
   return (
     <>
-      { posts.length > 0 &&
+      {posts.length > 0 && (
         <div>
-          {posts?.map(post => (
+          {posts?.map((post) => (
             <Post
               post={post}
               key={post._id}
@@ -16,22 +16,20 @@ const Posts = ({ posts }) => {
             />
           ))}
         </div>
-      }
-      {
-        posts.length === 0 &&
+      )}
+      {posts.length === 0 && (
         <div className="flex-center justify-center text-text dark:text-text-dark">
-            <p>No posts available</p>
+          <p>No posts available</p>
         </div>
-      }
-      {
-        clickedPostImage &&
+      )}
+      {clickedPostImage && (
         <LightBox
           imageURL={clickedPostImage}
           setClickedPostImage={setClickedPostImage}
         />
-      }
+      )}
     </>
   );
-}
- 
+};
+
 export default Posts;
